9f9103e192ef8622d2d08fdbb3a8c8696ae7493b
CarlosPadilla0 <Carlosepadillap8@hotmail.com>
2025-05-23 09:28:56 -0700

    Agregar configuración inicial de Maven y estructura del proyecto Spring Boot

---
+++ ApiRest-Eventos/src/main/java/com/tecnm/ApiRest_Eventos/ApiRestEventosApplication.java
@@ -0,0 +1,13 @@
+package com.tecnm.ApiRest_Eventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestEventosApplication.class, args);
+	}
+
+}
